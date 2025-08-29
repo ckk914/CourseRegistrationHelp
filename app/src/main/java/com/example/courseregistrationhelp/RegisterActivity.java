a6c462922d4b04010887516d9e2b7dfb2c73cc72
KyoungkonCho <50573460+ckk914@users.noreply.github.com>
2025-08-29 23:02:03 +0900

    add: register 관련 activity 추가

---
+++ app/src/main/java/com/example/courseregistrationhelp/RegisterActivity.java
@@ -0,0 +1,29 @@
+package com.example.courseregistrationhelp;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private ArrayAdapter adapter;
+    private Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register);
+
+        spinner = (Spinner) findViewById(R.id.majorSpinner);
+        adapter = ArrayAdapter.createFromResource(this, R.array.major, android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(adapter);
+
+    }
+}
