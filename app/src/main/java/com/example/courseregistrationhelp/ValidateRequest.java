bf04ba94fbc40d9f89d754f63f405d3177f2b27f
KyoungkonCho <50573460+ckk914@users.noreply.github.com>
2025-08-31 20:32:10 +0900

    add: ValidateRequest -> 계정 생성 시, id 검증

---
+++ app/src/main/java/com/example/courseregistrationhelp/ValidateRequest.java
@@ -0,0 +1,25 @@
+package com.example.courseregistrationhelp;
+
+
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidateRequest extends StringRequest{
+    final static private String URL = "http://seq0914.dothome.co.kr/StudentValidate.php";
+    private Map<String, String> parameters;
+
+    public ValidateRequest(String studentID, Response.Listener<String> listener){
+        super(Method.POST, URL, listener, null);
+
+        parameters = new HashMap<>();
+        parameters.put("studentID", studentID);
+    }
+    @Override
+    public Map<String, String> getParams(){
+        return parameters;
+    }
+}
