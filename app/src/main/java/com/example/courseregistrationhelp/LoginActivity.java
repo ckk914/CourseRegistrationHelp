dc620d063f5d58b9ec5dcf2270182c479b4f6ddc
KyoungkonCho <50573460+ckk914@users.noreply.github.com>
2025-09-15 21:20:48 +0900

    add: 로그인 request, 메인 엑티비티 추가

--- app/src/main/java/com/example/courseregistrationhelp/LoginActivity.java
+++ app/src/main/java/com/example/courseregistrationhelp/LoginActivity.java
@@ -3,16 +3,26 @@ package com.example.courseregistrationhelp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
 public class LoginActivity extends AppCompatActivity {
 
+    private AlertDialog alertDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,5 +37,61 @@ public class LoginActivity extends AppCompatActivity {
            LoginActivity.this.startActivity(registerIntent);
             }
         });
+
+        final EditText idText = (EditText) findViewById(R.id.idText);
+        final EditText passwordText = (EditText) findViewById(R.id.passwordText);
+        final Button loginButton = (Button) findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String studentId = idText.getText().toString();
+                String studentPassword = passwordText.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                     //
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if(success){
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                alertDialog = builder.setMessage("로그인에 성공했습니다.")
+                                        .setPositiveButton("확인",null)
+                                        .create();
+                                alertDialog.show();
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                LoginActivity.this.startActivity(intent);
+                                finish();
+                            }
+                            else{
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                alertDialog = builder.setMessage("계정을 다시 확인해주세요.")
+                                        .setNegativeButton("다시 시도",null)
+                                        .create();
+                                alertDialog.show();
+                            }
+                        }
+                        catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                LoginRequest loginRequest = new LoginRequest(studentId, studentPassword, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
+
+            }
+        });
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        if(alertDialog != null){
+            alertDialog.dismiss(); //종료 안되게 막음.
+            alertDialog = null;
+        }
+
     }
 }
