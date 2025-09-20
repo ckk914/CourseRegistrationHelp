e17a2f5c7318d150739df807e26cc72ee00c81ff
KyoungkonCho <50573460+ckk914@users.noreply.github.com>
2025-09-20 17:36:26 +0900

    add: fragment 추가 (course / schedule / statistics), logo, MainActivity 수정

--- app/src/main/java/com/example/courseregistrationhelp/MainActivity.java
+++ app/src/main/java/com/example/courseregistrationhelp/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.courseregistrationhelp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +20,52 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        final Button courseButton = findViewById(R.id.courseButton);
+        final Button scheduleButton = findViewById(R.id.scheduleButton);
+        final Button statisticsButton = findViewById(R.id.statisticsButton);
+        final LinearLayout notice = (LinearLayout) findViewById(R.id.notice);
+
+        courseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                notice.setVisibility(View.GONE); //공지사항 부분 숨김.
+                courseButton.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+                scheduleButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                statisticsButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment, new CourseFragment());  //화면 교체
+                fragmentTransaction.commit();                                      //커밋팅
+            }
+        });
+        //통계
+        statisticsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                notice.setVisibility(View.GONE); //공지사항 부분 숨김.
+                courseButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                scheduleButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                statisticsButton.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment, new StatisticsFragment());  //화면 교체
+                fragmentTransaction.commit();                                      //커밋팅
+            }
+        });
+        //스케줄 버튼
+        scheduleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                notice.setVisibility(View.GONE); //공지사항 부분 숨김.
+                courseButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                scheduleButton.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+                statisticsButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment, new ScheduleFragment());  //화면 교체
+                fragmentTransaction.commit();                                      //커밋팅
+            }
         });
     }
 }
