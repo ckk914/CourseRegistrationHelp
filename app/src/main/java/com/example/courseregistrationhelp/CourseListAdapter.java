2e6eb0c3a56844ac84d082b5115e1166c75cb861
KyoungkonCho <50573460+ckk914@users.noreply.github.com>
2025-10-09 16:33:42 +0900

    add: 계정별 강의 추가 가능 하도록 구현

--- app/src/main/java/com/example/courseregistrationhelp/CourseListAdapter.java
+++ app/src/main/java/com/example/courseregistrationhelp/CourseListAdapter.java
@@ -1,20 +1,34 @@
 package com.example.courseregistrationhelp;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 import java.util.List;
 
 public class CourseListAdapter extends BaseAdapter {
     private Context context;
     private List<Course> courseList;
+    private Fragment parent;
 
-    public CourseListAdapter( Context context,List<Course> courseList) {
+    public CourseListAdapter( Context context,List<Course> courseList, Fragment parent) {
         this.courseList = courseList;
         this.context = context;
+        this.parent = parent;
     }
 
     @Override
@@ -33,7 +47,7 @@ public class CourseListAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int i, View convertView, ViewGroup parent) {
+    public View getView(final int i, View convertView, ViewGroup parent) {
         View v = View.inflate(context, R.layout.course, null);
         TextView courseGrade = (TextView) v.findViewById(R.id.courseGrade);
         TextView courseTitle = (TextView) v.findViewById(R.id.courseTitle);
@@ -59,10 +73,72 @@ public class CourseListAdapter extends BaseAdapter {
         else {
             coursePersonnel.setText("제한 인원: "+ courseList.get(i).getCoursePersonnel()+"명");
         }
-        courseProfessor.setText(courseList.get(i).getCourseProfessor()+" 교수님");
+        if(courseList.get(i).getCourseProfessor().equals("")) {
+            courseProfessor.setText("개인 연구");
+        }
+        else{
+            courseProfessor.setText(courseList.get(i).getCourseProfessor()+" 교수님");
+        }
+
         courseTime.setText(courseList.get(i).getCourseTime()+"");
 
         v.setTag(courseList.get(i).getCourseID());
+
+        Button addButton = (Button) v.findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            String studentID = MainActivity.studentID;
+            //
+            Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String s) {
+                        Log.d("DEBUG", "강의추가 서버 응답: " + s);
+                        try {
+                            JSONObject jsonResponse = new JSONObject(s);
+                            boolean success = jsonResponse.getBoolean("success");
+
+                            if (success) {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(parent.getContext());
+                                AlertDialog dialog = builder.setMessage("강의가 추가되었습니다.")
+                                        .setPositiveButton("확인", null)
+                                        .create();
+                                dialog.show();
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(parent.getContext());
+                                AlertDialog dialog = builder.setMessage("강의 추가 실패했습니다.")
+                                        .setNegativeButton("확인", null)
+                                        .create();
+                                dialog.show();
+                                Log.d("DEBUG", "강의추가 실패");
+                            }
+                        } catch (Exception e) {
+                            Log.e("DEBUG", "강의추가 JSON 파싱 오류: " + e.getMessage());
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                Response.ErrorListener errorListener = new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("DEBUG", "강의 추가 오류: " + error.getMessage());
+                        AlertDialog.Builder builder = new AlertDialog.Builder(parent.getContext());
+                        AlertDialog dialog = builder.setMessage("강의 추가 실패했습니다.")
+                                .setPositiveButton("확인", null)
+                                .create();
+                        dialog.show();
+                    }
+                };
+                Log.d("kk-DEBUG", "studentID: " + studentID);
+                Log.d("kk-DEBUG", "courseID: " + courseList.get(i).getCourseID());
+                AddRequest addRequest = new AddRequest(studentID, courseList.get(i).getCourseID()+"",responseListener);
+                RequestQueue queue = Volley.newRequestQueue(parent.getContext());
+                queue.add(addRequest);
+            //
+            }
+        });
+
         return v;
     }
 }
