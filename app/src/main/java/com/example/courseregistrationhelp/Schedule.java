c70659c5f62b0fd0e9fb1ca178ac85ad951fcdd5
KyoungkonCho <50573460+ckk914@users.noreply.github.com>
2025-10-11 18:02:41 +0900

    add: - Schedule addSchedule 오버로딩 함수 추가      - 추가된 것을 기반으로 세팅할 수 있는 setting 함수 추가

--- app/src/main/java/com/example/courseregistrationhelp/Schedule.java
+++ app/src/main/java/com/example/courseregistrationhelp/Schedule.java
@@ -1,5 +1,8 @@
 package com.example.courseregistrationhelp;
 
+import android.content.Context;
+import android.widget.TextView;
+
 public class Schedule {
     private String monday[] = new String[14]; //0-13교시
     private String tuesday[] = new String[14];
@@ -181,5 +184,115 @@ public class Schedule {
         } //금
 
         return true; //중복 아님!
+    }
+
+    //->
+    public void addSchedule(String scheduleText, String courseTitle, String courseProfessor){
+        String professor;
+        if(courseProfessor.equals("")){
+            professor = "";
+        }
+        else{
+            professor = "(" + courseProfessor + ")";
+        }
+
+        int temp;
+        //월:[3][4][5]화:[4][5]
+        if((temp = scheduleText.indexOf("월")) > -1){
+            temp+=2;
+            int startPoint = temp;
+            int endPoint  = temp;
+            for(int i = temp; i < scheduleText.length() && scheduleText.charAt(i) != ':';i++){
+                if(scheduleText.charAt(i) == '['){
+                    startPoint = i;
+                }
+                if(scheduleText.charAt(i) == ']'){
+                    endPoint = i;
+                    monday[Integer.parseInt(scheduleText.substring(startPoint+1,endPoint))] = courseTitle + professor;
+                }
+            }
+        } //월
+        if((temp = scheduleText.indexOf("화")) > -1){
+            temp+=2;
+            int startPoint = temp;
+            int endPoint  = temp;
+            for(int i = temp; i < scheduleText.length() && scheduleText.charAt(i) != ':';i++){
+                if(scheduleText.charAt(i) == '['){
+                    startPoint = i;
+                }
+                if(scheduleText.charAt(i) == ']'){
+                    endPoint = i;
+                    tuesday[Integer.parseInt(scheduleText.substring(startPoint+1,endPoint))] = courseTitle + professor;
+                }
+            }
+        } //화
+        if((temp = scheduleText.indexOf("수")) > -1){
+            temp+=2;
+            int startPoint = temp;
+            int endPoint  = temp;
+            for(int i = temp; i < scheduleText.length() && scheduleText.charAt(i) != ':';i++){
+                if(scheduleText.charAt(i) == '['){
+                    startPoint = i;
+                }
+                if(scheduleText.charAt(i) == ']'){
+                    endPoint = i;
+                    wednesday[Integer.parseInt(scheduleText.substring(startPoint+1,endPoint))] = courseTitle + professor;
+                }
+            }
+        } //수
+        if((temp = scheduleText.indexOf("목")) > -1){
+            temp+=2;
+            int startPoint = temp;
+            int endPoint  = temp;
+            for(int i = temp; i < scheduleText.length() && scheduleText.charAt(i) != ':';i++){
+                if(scheduleText.charAt(i) == '['){
+                    startPoint = i;
+                }
+                if(scheduleText.charAt(i) == ']'){
+                    endPoint = i;
+                    thursday[Integer.parseInt(scheduleText.substring(startPoint+1,endPoint))] = courseTitle + professor;
+                }
+            }
+        } //목
+        if((temp = scheduleText.indexOf("금")) > -1){
+            temp+=2;
+            int startPoint = temp;
+            int endPoint  = temp;
+            for(int i = temp; i < scheduleText.length() && scheduleText.charAt(i) != ':';i++){
+                if(scheduleText.charAt(i) == '['){
+                    startPoint = i;
+                }
+                if(scheduleText.charAt(i) == ']'){
+                    endPoint = i;
+                    friday[Integer.parseInt(scheduleText.substring(startPoint+1,endPoint))] = courseTitle + professor;
+                }
+            }
+        } //금
+    } // end addSchedule
+    //<-
+    //입력받은 내용들 표현해주기 위한 세팅 함수
+    public void setting(TextView[] monday, TextView[] tuesday, TextView[] wednesday, TextView[] thursday, TextView[] friday, Context context){
+        for(int i = 0; i < 14; i++) {
+         if(!this.monday[i].equals("")){
+             monday[i].setText(this.monday[i]);
+             monday[i].setTextColor(context.getResources().getColor(R.color.colorPrimary));
+         }
+            if (!this.tuesday[i].equals("")) {
+                tuesday[i].setText(this.tuesday[i]);
+                tuesday[i].setTextColor(context.getResources().getColor(R.color.colorPrimary));
+            }
+            if (!this.wednesday[i].equals("")) {
+                wednesday[i].setText(this.wednesday[i]);
+                wednesday[i].setTextColor(context.getResources().getColor(R.color.colorPrimary));
+            }
+            if (!this.thursday[i].equals("")) {
+                thursday[i].setText(this.thursday[i]);
+                thursday[i].setTextColor(context.getResources().getColor(R.color.colorPrimary));
+            }
+            if (!this.friday[i].equals("")) {
+                friday[i].setText(this.friday[i]);
+                friday[i].setTextColor(context.getResources().getColor(R.color.colorPrimary));
+            }
+        }
     }
 }
