f3737f5a945f79da11f3d311638661311b2257a1
KyoungkonCho <50573460+ckk914@users.noreply.github.com>
2025-10-20 01:33:27 +0900

    add: Course 파라미터 추가, Rival      getter,setter,생성자 add

--- app/src/main/java/com/example/courseregistrationhelp/Course.java
+++ app/src/main/java/com/example/courseregistrationhelp/Course.java
@@ -18,7 +18,7 @@ public class Course {
     String courseProfessor; //강의 교수
     String courseTime; //강의 시간
     String courseRoom; // 강의실
-
+    int courseRival; //강의 경쟁자 수
     public void setCourseID(int courseID) {
         this.courseID = courseID;
     }
@@ -130,6 +130,25 @@ public class Course {
     public String getCourseRoom() {
         return courseRoom;
     }
+
+    public int getCourseRival() {
+        return courseRival;
+    }
+
+    public void setCourseRival(int courseRival) {
+        this.courseRival = courseRival;
+    }
+
+    public Course(int courseID, String courseGrade, String courseTitle, int courseDivide, int coursePersonnel, int courseRival) {
+        this.courseID = courseID;
+        this.courseGrade = courseGrade;
+        this.courseTitle = courseTitle;
+        this.courseDivide = courseDivide;
+        this.coursePersonnel = coursePersonnel;
+        this.courseRival = courseRival;
+    }
+
+
 
     public Course(int courseID, String courseUniversity, String courseYear, String courseTerm, String courseArea, String courseMajor, String courseGrade, String courseTitle, int courseCredit, int courseDivide, int coursePersonnel, String courseProfessor, String courseTime, String courseRoom) {
         this.courseID = courseID;
