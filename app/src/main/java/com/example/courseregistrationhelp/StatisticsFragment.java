97e1dbb67cd605f954f53ded53f64e3017942598
KyoungkonCho <50573460+ckk914@users.noreply.github.com>
2025-10-20 23:41:36 +0900

    add: StatisticsCourseListAdapter, StatisticsFragment 추가

--- app/src/main/java/com/example/courseregistrationhelp/StatisticsFragment.java
+++ app/src/main/java/com/example/courseregistrationhelp/StatisticsFragment.java
@@ -1,12 +1,28 @@
 package com.example.courseregistrationhelp;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -53,6 +69,109 @@ public class StatisticsFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+    }
+
+    private ListView courseListView;
+    private StatisticsCourseListAdapter adapter;
+    private List<Course> courseList;
+    int totalCredit = 0;
+
+    @Override
+    public void onActivityCreated(Bundle b){
+        super.onActivityCreated(b);
+        courseListView = (ListView) getActivity().findViewById(R.id.courseListView);
+        courseList = new ArrayList<Course>();
+        adapter = new StatisticsCourseListAdapter(getContext().getApplicationContext(), courseList, this);
+        courseListView.setAdapter(adapter);
+        new BackgroundTask().execute();
+    }
+
+    class BackgroundTask extends AsyncTask<Void, Void, String> {
+
+        String target; // 접속할 주소
+
+        @Override
+        protected void onPreExecute() {
+            try {
+                target = "http://seq0914.dothome.co.kr/StatisticsCourseList.php?studentID=" + URLEncoder.encode(MainActivity.studentID, "UTF-8");
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            try {
+                URL url = new URL(target);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String temp;
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ((temp = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(temp + "\n");   //읽여서 한줄씩 추
+                }
+                //사용 해제
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+
+                return stringBuilder.toString().trim();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return "";
+        }
+
+        @Override
+        public void onProgressUpdate(Void... values) {
+            super.onProgressUpdate();
+        }
+
+        //해당 결과 처리용
+        @Override
+        public void onPostExecute(String result) {
+            try {
+                // 실제로 서버에서 받은 응답 내용 로그 찍기
+                Log.d("KK->ScheduleFragment", "Server response: " + result);
+
+                JSONObject jsonObject = new JSONObject(result);
+                //response에 각각의 공지사항들이 있음.
+                JSONArray jsonArray = jsonObject.getJSONArray("response");
+
+                int count = 0;
+                int courseID;
+                String courseGrade;
+                String courseTitle;
+                int courseDivide;
+                int coursePersonnel;
+                int courseRival;
+
+                while (count < jsonArray.length()) {
+                    //카운트에 맞는 것 가져옴!
+                    JSONObject object = jsonArray.getJSONObject(count);
+                    //해당값 가져옴!
+                    courseID = object.getInt("courseID");
+                    courseGrade = object.getString("courseGrade");
+                    courseTitle = object.getString("courseTitle");
+                    courseDivide = object.getInt("courseDivide");
+                    coursePersonnel = object.getInt("coursePersonnel");
+                    courseRival = object.getInt("COUNT(SCHEDULE.courseID)");
+                    totalCredit += object.getInt("courseCredit");
+                    courseList.add(new Course(courseID, courseGrade, courseTitle, courseDivide, coursePersonnel, courseRival));
+                    count++;
+                } //end while
+                adapter.notifyDataSetChanged();
+                TextView credit = (TextView) getView().findViewById(R.id.totalCredit);
+                credit.setText(totalCredit + "학점");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }       //
     }
 
     @Override
